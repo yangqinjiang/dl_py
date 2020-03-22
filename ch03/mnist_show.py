c7b72fd2a3741ad47e24678e6cd515979116daf7
yangqinjiang-macbookpro <yangqinjiang@qq.com>
2020-03-22 16:14:04 +0800

    显示手写数字的图片

--- ch03/mnist_show.py
+++ ch03/mnist_show.py
@@ -13,9 +13,18 @@ def img_show(img):
 (x_train,t_train),(x_test,t_test) = load_mnist(flatten=True,normalize=False)
 img = x_train[0]
 label = t_train[0]
-print(label)
-print(img.shape)
-img = img.reshape(28,28)
-print(img.shape)
+print(label)  # 5
+print(img.shape) # (784 ,)
+img = img.reshape(28,28)# 把图像的形状变成原来的尺寸
+print(img.shape) #(28,28)
+print('show image')
 
 img_show(img)
+
+# 调用img_show函数出错: 
+#   FSPathMakeRef(/Applications/Preview.app) failed with error
+# https://blog.csdn.net/qq_35793285/article/details/103028425
+# 解决方式:
+# ln -s /System/Applications/Preview.app /Applications/Preview.app
+
+
